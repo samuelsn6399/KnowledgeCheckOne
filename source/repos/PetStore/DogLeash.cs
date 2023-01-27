99888e61a1fec93b46230533e9212119e7d0e870
Samuel Newport <samuelsn6399@yahoo.com>
2023-01-26 23:08:00 -0600

    new changes

---
+++ source/repos/PetStore/DogLeash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    public class DogLeash : Product
+    {
+
+        public int LengthInches = 0;
+        public string Material = "";
+
+    }
+}
---
+++ source/repos/PetStore/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    public class Product
+    {
+
+        public string Name = "";
+        public decimal Price;
+        public int Quantity;
+        public string Description = "";
+
+
+    }
+}
